ba8ddbf3e3f928601b0809952e1cf5e32c59c7ea
atiQkhalid <39992488+atiQkhalid@users.noreply.github.com>
2021-09-29 10:22:42 +0300

    open source

--- app/src/main/java/com/example/mynotes/repositories/NoteRepository.kt
+++ app/src/main/java/com/example/mynotes/repositories/NoteRepository.kt
@@ -8,11 +8,11 @@ class NoteRepository(private val noteDao: NoteDao) {
 
     val allNotes: LiveData<List<Note>> = noteDao.getAllNotes()
 
-    suspend fun insert(note: Note){
+    suspend fun insert(note: Note) {
         noteDao.insert(note)
     }
 
-    suspend fun delete(note: Note){
+    suspend fun delete(note: Note) {
         noteDao.delete(note)
     }
 
